c05a3c5c00bfc7cb8c2b35dca0c4eae9ee8a86ec
jefferson-gbarbosa <jeffersonqx@gmail.com>
2023-09-22 11:02:53 -0300

    api scripts introduction

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-/*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navLink = document.querySelectorAll('.nav-link');
+const form = document.querySelector('form');
+const input = document.getElementById('input-field');
+const linkContainer = document.querySelector('.links');
+
+const API_URL = 'https://api.shrtco.de/v2/';
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
@@ -17,4 +21,24 @@ function linkAction(){
     navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click',linkAction));
+
+form.addEventListener("submit", async (event) =>{
+    event.preventDefault();
+
+    const inputUrlValue = input.value;
+    const result = await fetch(`${API_URL}shorten?url=${inputUrlValue}`);
+    // const result = await fetch(`https://api.shrtco.de/v2/shorten?url=${inputUrlValue}`)
+    const shortedResult = await result.json();
+    console.log(shortedResult);
+    linkContainer.innerHTML = 
+    `
+        <div class="link">
+            <p class="title-link-origin">${shortedResult.result.original_link}</p>
+            <div class="shorted">
+            <p class="title-link-shorted">${shortedResult.result.short_link}</p>
+            <button class="btn-copy">Copy</button>
+            </div>
+        </div>
+    `;
+})
 
